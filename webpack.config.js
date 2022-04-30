d8ff10621df6cf4030226eb91241131a4f418b37
luv-jeri <luvvjeri@gmail.com>
2022-04-30 18:04:45 +0530

    -

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development', // production
   entry: {
-    bundle :   path.join(__dirname, 'src/index.js'),
+    bundle :  path.join(__dirname, 'src/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
